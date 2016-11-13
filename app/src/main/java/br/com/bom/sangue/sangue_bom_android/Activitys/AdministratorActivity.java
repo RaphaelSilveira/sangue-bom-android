c920bf9b2cde1e4a10313b64404ac506f33e53c0
Raphael Silveira <raphajts@gmail.com>
2016-11-13 18:59:13 -0200

    Finalização do formulário Quero Ser Doador

--- app/src/main/java/br/com/bom/sangue/sangue_bom_android/Activitys/AdministratorActivity.java
+++ app/src/main/java/br/com/bom/sangue/sangue_bom_android/Activitys/AdministratorActivity.java
@@ -1,10 +1,7 @@
-package br.com.bom.sangue.sangue_bom_android;
+package br.com.bom.sangue.sangue_bom_android.Activitys;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,6 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import br.com.bom.sangue.sangue_bom_android.R;
+//import br.com.jansenfelipe.androidmask.MaskEditTextChangedListener;
 
 public class AdministratorActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -73,5 +74,13 @@ public class AdministratorActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void insertMasks () {
+        EditText cpf = (EditText) findViewById(R.id.input_cpf);
+
+        //MaskEditTextChangedListener maskCpf = new MaskEditTextChangedListener("###.###.###-##", cpf);
+
+        //cpf.addTextChangedListener(maskCpf);
     }
 }
