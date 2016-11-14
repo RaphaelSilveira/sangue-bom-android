f1b1d873817b6aa80078df439091b31412013379
Raphael Silveira <raphajts@gmail.com>
2016-11-14 21:16:03 -0200

    Criação das constantes e do callback de RegistrationVerification

---
+++ app/src/main/java/br/com/bom/sangue/sangue_bom_android/Constants/Endpoints.java
@@ -0,0 +1,11 @@
+package br.com.bom.sangue.sangue_bom_android.Constants;
+
+/**
+ * Created by Raphael on 14/11/2016.
+ */
+public final class Endpoints {
+
+    private static final String ENDPOINT = "192.168.0.13";
+
+    public static final String REGISTRATION_VERIFICATION = ENDPOINT + "/registration-verification";
+}
