5797ce148b2576e8203a37525dd9d46e041d4aaa
Raphael Silveira <raphajts@gmail.com>
2016-11-23 22:09:52 -0200

    Finalização da confirmação de doação

--- app/src/main/java/br/com/bom/sangue/sangue_bom_android/Activitys/DetailsIntentDonationActivity.java
+++ app/src/main/java/br/com/bom/sangue/sangue_bom_android/Activitys/DetailsIntentDonationActivity.java
@@ -1,5 +1,7 @@
 package br.com.bom.sangue.sangue_bom_android.Activitys;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,13 +13,21 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import org.json.JSONException;
+
+import br.com.bom.sangue.sangue_bom_android.Callbacks.IntentDonationCallback;
 import br.com.bom.sangue.sangue_bom_android.Entities.Address;
 import br.com.bom.sangue.sangue_bom_android.Entities.BloodDonator;
 import br.com.bom.sangue.sangue_bom_android.Entities.IntentDonation;
 import br.com.bom.sangue.sangue_bom_android.Entities.Telephone;
+import br.com.bom.sangue.sangue_bom_android.Providers.IntentDonationProvider;
 import br.com.bom.sangue.sangue_bom_android.R;
 
 public class DetailsIntentDonationActivity extends AppCompatActivity {
+
+    IntentDonation intentDonation = new IntentDonation();
+
+    IntentDonationProvider intentDonationProvider = new IntentDonationProvider();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,11 @@ public class DetailsIntentDonationActivity extends AppCompatActivity {
         final Button button = (Button) findViewById(R.id.confirm_donation);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //TODO
+                try {
+                    setGrantDate();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -39,7 +53,7 @@ public class DetailsIntentDonationActivity extends AppCompatActivity {
 
     private void populateViews (String intentDonationJson) {
         Gson gson = new Gson();
-        IntentDonation intentDonation = gson.fromJson(intentDonationJson, IntentDonation.class);
+        intentDonation = gson.fromJson(intentDonationJson, IntentDonation.class);
 
         TextView name = (TextView) findViewById(R.id.name_detail);
         name.setText(intentDonation.getBloodDonator().getName());
@@ -62,6 +76,26 @@ public class DetailsIntentDonationActivity extends AppCompatActivity {
 
         TextView email = (TextView) findViewById(R.id.email_detail);
         email.setText(intentDonation.getBloodDonator().getEmail());
+    }
+
+    private void setGrantDate () throws JSONException {
+        final ProgressDialog progressDialog = ProgressDialog.show(this, "Aguarde...", "Concluindo a ação...", true);
+        progressDialog.setCancelable(true);
+
+        IntentDonationCallback intentDonationCallback = new IntentDonationCallback() {
+            @Override
+            public void success() {
+                progressDialog.dismiss();
+                comeBackIntentDonationList();
+            }
+        };
+
+        intentDonationProvider.setGrantDate(intentDonation, this, intentDonationCallback);
+    }
+
+    private void comeBackIntentDonationList () {
+        Intent intent = new Intent(this, ActiveIntentDonationsActivity.class);
+        startActivity(intent);
     }
 
 }
